5ba6aa7ea70f53355a0bb8df9063d4e8af1d2b08
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2025-07-24 12:46:05 +0200

    feat(ruby): add each method to InstanceMethods and overwriting method_missing of CsvRow to retrieve value of a row based on header using metaprogramming

--- ruby/ruby/day3/csv_metaprogramming.rb
+++ ruby/ruby/day3/csv_metaprogramming.rb
@@ -1,3 +1,15 @@
+# Modify the CSV application to support an each method to return a CsvRow object.
+# Use method_missing on that CsvRow to return the value for the column for a given heading.
+#
+# For example, for the file:
+# one, two
+# lions, tigers
+#
+# allow an API that works like this:
+# csv = RubyCsv.new
+# csv.each {|row| puts row.one}
+# This should print "lions".
+
 module ActsAsCsv
   def self.included(base)
     base.extend ClassMethods
@@ -12,7 +24,7 @@ module ActsAsCsv
   module InstanceMethods
     def read
       @csv_contents = []
-      filename = self.class.to_s.downcase + '.txt'
+      filename = 'day3/' + self.class.to_s.downcase + '.txt'
       file = File.new(filename)
       @headers = file.gets.chomp.split(', ')
       file.each do |row|
@@ -23,6 +35,9 @@ module ActsAsCsv
     def initialize
       read
     end
+    def each
+      csv_contents.each { |line| yield CsvRow.new(@headers, line) }
+    end
   end
 end
 
@@ -30,3 +45,21 @@ class RubyCsv # no inheritance! You can mix it in
   include ActsAsCsv
   acts_as_csv
 end
+
+class CsvRow
+  def initialize( headers = [], fields = [] )
+    @fields = {}
+    headers.each_with_index do |header, index|
+      @fields[header] = fields[index]
+    end
+  end
+
+  def method_missing name, *args
+    @fields[name.to_s] || ''
+  end
+end
+
+csv = RubyCsv.new
+csv.each {|row| p row}
+csv.each {|row| p row.one}
+csv.each {|row| p row.not_existing}
