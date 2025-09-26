eb12a4012c679f77bb9901a40007a41783f07b1f
Mauro Magnacavallo <mmagnacavallo@vitesicure.it>
2025-09-26 19:03:47 +0200

    refactor(prolog): simplify match of univ result. credits to @daniele_megna

--- prolog/day1/books.pl
+++ prolog/day1/books.pl
@@ -24,16 +24,13 @@ test(Title, Function) :-
 
 test(Title, Function, Expected) :-
     (call(Function) ->
-        Function =.. [_,_|ArrayResult],
-        extract(ArrayResult, Result),
+        Function =.. [_,_,Result],
         (Result == Expected ->
             format('✓ ~w: PASS~n', [Title])
         ;   format('✗ ~w: FAIL~n - Expected: ~w~n - Received: ~w~n', [Title, Expected, Result])
         )
     ;   format('✗ ~w: FAIL (failed to execute function)~n', [Title])
     ).
-
-extract([X], X).
 
 run_tests :-
     test('Uncle Bob book', wroteBook(martin, clean_code)),
